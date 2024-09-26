93d8b1ebd4c3054ae25a174de57166808d745916
amelazzi <amelias.azzi@gmail.com>
2024-09-26 13:26:03 +0200

    💄 add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from "@/components/navbar/navbar";
+
 export default function Home() {
   return (
     <div>
-      <h1> Welcome to Bob portfolio </h1>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <nav>
+        <a> Home </a>
+        <a> About </a>
+        <a> Education </a>
+        <a> Formations </a>
+        <a> Experiences </a>
+        <a> Volunteering </a>
+      </nav>
+      <a> Contact </a>
+    </div>
+  )
+}
+
+export default Navbar
